9f0ed02bde5e5937e1f4697e64d77e19562cd1b8
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-10 17:10:27 +1300

    Use type system to create post blog

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use rust_oop_types_blog_post::Post;
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    
+    let post = post.request_review();
+
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    
+}
